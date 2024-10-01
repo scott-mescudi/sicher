ad58e3c0456b27d6701a635ed62909139d827143
script kiddie <anish.chittu2007@gmail.com>
2024-10-01 23:26:27 +0200

    added file filtering

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"gobackup/src"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"os"
 )
+
 //TODO 1: add concurrency to clean func
 //TODO 2: make each file a go routine with limit of 10 and 100mb per file
 //TODO 3: implement worker pool for TODO 2
@@ -18,28 +20,51 @@ func main() {
 		"dstf",
 	}
 
+	start := time.Now()
 	s.StartBackup()
+	fmt.Printf("Elapsed: %v\n", time.Since(start))
 }
 
+var NotAllowedFiles = map[string]bool{
+	"test.exe": true,
+}
 
-func(s backup) StartBackup(){
+var NotAllowedExt = map[string]bool{
+	".sigma": true,
+}
+
+func (s backup) StartBackup() {
 	var srcfiles = make(map[string]bool)
 	defer src.Clean(s.srcDir, s.dstDir)
 
 	filepath.WalkDir(s.srcDir, func(path string, d os.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-			if path != s.srcDir{
-				srcfiles[path] = true
-			}
-			
-			return nil
+		if err != nil {
+			return err
+		}
+
+		if _, ok := NotAllowedFiles[path]; ok {
+			return filepath.SkipDir
+		}
+
+		ext := filepath.Ext(path)
+		if _, ok := NotAllowedExt[ext]; ok {
+			return filepath.SkipDir
+		}
+
+		if path != s.srcDir {
+			srcfiles[path] = true
+		}
+
+		return nil
 	})
 
+	work(srcfiles, s)
+
+}
+
+func work(srcfiles map[string]bool, s backup){
 	for i := range srcfiles {
 		x := strings.TrimPrefix(i, s.srcDir)
-		
 		dstfile := filepath.Join(s.dstDir, x)
 		srcfile := filepath.Join(i)
 
@@ -49,21 +74,13 @@ func(s backup) StartBackup(){
 		}
 
 		err = src.CopyFile(srcfile, dstfile, 1024)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 		}
 	}
-
-
 }
 
-type backup struct{
+type backup struct {
 	srcDir string
 	dstDir string
 }
-
-
-
-
-
-
